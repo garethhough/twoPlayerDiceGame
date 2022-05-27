5fe081e20c59b37ea8e94e1c35971924e8567a70
garethhough <garethhough@googlemail.com>
2022-05-27 17:12:34 +0100

    Added reset function

--- diceGame.js
+++ diceGame.js
@@ -6,6 +6,8 @@ let newGameButton = document.getElementById("newGameButton");
 
 let rollButtonOne = document.getElementById("rollButtonOne");
 
+let holdButtonOne = document.getElementById("holdButtonOne");
+
 let numberRolledOne = document.getElementById("numberRolledOne");
 
 let scoreOne = document.getElementById("scoreOne");
@@ -16,12 +18,13 @@ let playerOneScore = 0;
 
 let rollButtonTwo = document.getElementById("rollButtonTwo");
 
+let holdButtonTwo = document.getElementById("holdButtonTwo");
+
 let numberRolledTwo = document.getElementById("numberRolledTwo");
 
 let scoreTwo = document.getElementById("scoreTwo");
 
 let playerTwoScore = 0;
-
 
 //Player One Game
 
@@ -35,7 +38,10 @@ rollButtonOne.addEventListener("click", function () {
 // Keeps track of player one's score
 function scoreFunctionOne(rolledNumOne) {
     if (playerOneScore <= 1 && rolledNumOne == 1) {
-        alert("You rolled a 1 first, scores will now be reset !");
+        alert("You rolled a 1 first, scores will now be reset!");
+        reset("playerOne");
+    } else if (playerOneScore >= 2 && playerOneScore <= 20 && rolledNumOne == 1) {
+        alert("You rolled a 1, You lose!");
         newGame();
 // If score is less than 20, roll again
     } else if (playerOneScore <= 20) {
@@ -43,12 +49,11 @@ function scoreFunctionOne(rolledNumOne) {
         scoreOne.innerHTML = playerOneScore;
 // If players score is greater than 20, you win
     } else {
-        alert("You scored more than 20, You win!");
+        alert("You scored more than 20, Player One wins!");
         newGame();
 
     }
 }
-
 
 //Player Two Game
 
@@ -59,10 +64,13 @@ rollButtonTwo.addEventListener("click", function () {
     scoreFunctionTwo(rolledNumTwo);
 });
 
-// Keeps track of player two's score
+// Keeps track of player Two's score
 function scoreFunctionTwo(rolledNumTwo) {
     if (playerTwoScore <= 1 && rolledNumTwo == 1) {
-        alert("You rolled a 1 first, scores will now be reset !");
+        alert("You rolled a 1 first, scores will now be reset!");
+        reset("playerTwo");
+    } else if (playerTwoScore >= 2 && playerTwoScore <= 20 && rolledNumTwo == 1) {
+        alert("You rolled a 1, You lose!");
         newGame();
 // If score is less than 20, roll again
     } else if (playerTwoScore <= 20) {
@@ -70,12 +78,10 @@ function scoreFunctionTwo(rolledNumTwo) {
         scoreTwo.innerHTML = playerTwoScore;
 // If players score is greater than 20, you win
     } else {
-        alert("You scored more than 20, You win!");
+        alert("You scored more than 20, Player Two wins!");
         newGame();
-
     }
 }
-
 
 // Clears stats
 function newGame() {
@@ -85,8 +91,48 @@ function newGame() {
     scoreTwo.innerHTML = null;
     numberRolledTwo.innerHTML = null;
     playerTwoScore = 0;
+    rollButtonOne.removeAttribute("disabled");
+    holdButtonOne.removeAttribute("disabled");
+    rollButtonTwo.setAttribute("disabled", "disabled");
+    holdButtonTwo.setAttribute("disabled", "disabled");
 }
 
-newGameButton.addEventListener("click", function (){
+function reset(player) {
+    scoreOne.innerHTML = null;
+    numberRolledOne.innerHTML = null;
+    playerOneScore = 0;
+    scoreTwo.innerHTML = null;
+    numberRolledTwo.innerHTML = null;
+    playerTwoScore = 0;
+    if (player === "playerOne") {
+        rollButtonTwo.removeAttribute("disabled");
+        holdButtonTwo.removeAttribute("disabled");
+        rollButtonOne.setAttribute("disabled", "disabled");
+        holdButtonOne.setAttribute("disabled", "disabled");
+    } else if (player === "playerTwo") {
+        rollButtonOne.removeAttribute("disabled");
+        holdButtonOne.removeAttribute("disabled");
+        rollButtonTwo.setAttribute("disabled", "disabled");
+        holdButtonTwo.setAttribute("disabled", "disabled");
+    } else {
+        console.log("play on")
+    }
+}
+
+newGameButton.addEventListener("click", function () {
     newGame();
+});
+
+holdButtonOne.addEventListener("click", function () {
+    rollButtonOne.setAttribute("disabled", "disabled");
+    holdButtonOne.setAttribute("disabled", "disabled");
+    holdButtonTwo.removeAttribute("disabled");
+    rollButtonTwo.removeAttribute("disabled");
+});
+
+holdButtonTwo.addEventListener("click", function () {
+    rollButtonTwo.setAttribute("disabled", "disabled");
+    holdButtonTwo.setAttribute("disabled", "disabled");
+    holdButtonOne.removeAttribute("disabled");
+    rollButtonOne.removeAttribute("disabled");
 });
